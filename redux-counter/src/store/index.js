f80b242f46aa5d762ba3d34a0d2348de01320137
Manav2031 <manavmehta2031@gmail.com>
2024-02-14 22:56:55 +0530

    final commit

--- redux-counter/src/store/index.js
+++ redux-counter/src/store/index.js
@@ -1,6 +1,19 @@
 import {createStore} from 'redux';
 
-const reducerFn = (state={counter:10}, action) => {
+const reducerFn = (state={counter:0}, action) => {
+    //Limitations
+    //1. Should be a synchronous function
+    //2. We should not mutate the original state.
+
+    if(action.type==="INC")
+    return {counter:state.counter+1};
+
+    if(action.type==="DEC")
+    return {counter:state.counter-1};
+
+    if(action.type==="ADD")
+    return {counter:state.counter+action.payload};
+
     return state;
 };
 
